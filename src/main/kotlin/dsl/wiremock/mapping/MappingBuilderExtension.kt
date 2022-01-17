e5bf4c2c5556623edae2e3b68108c81192bf3d32
Valentin Goncharov <goncharov.valentin@gmail.com>
2022-01-17 19:24:24 +0100

    - added MappingBuilder extension functions - added tests

---
+++ src/main/kotlin/dsl/wiremock/mapping/MappingBuilderExtension.kt
@@ -0,0 +1,26 @@
+package dsl.wiremock.mapping
+
+import com.github.tomakehurst.wiremock.client.MappingBuilder
+import dsl.wiremock.request.Cookie
+import dsl.wiremock.request.Header
+import dsl.wiremock.request.Parameter
+import dsl.wiremock.request.RequestBodyPattern
+
+fun <T: MappingBuilder> T.withHeaders(headers: List<Header>): T {
+    headers.forEach { withHeader(it.name, it.pattern) }
+    return this
+}
+
+fun <T: MappingBuilder> T.withCookies(cookies: List<Cookie>): T {
+    cookies.forEach { withCookie(it.name, it.pattern) }
+    return this
+}
+
+fun <T: MappingBuilder> T.withQueryParams(params: List<Parameter>): T {
+    params.forEach { withQueryParam(it.name, it.pattern) }
+    return this
+}
+fun <T: MappingBuilder> T.withRequestBodyPatterns(bodyPatterns: List<RequestBodyPattern>): T {
+    bodyPatterns.forEach { withRequestBody(it.pattern) }
+    return this
+}
---
+++ src/test/kotlin/dsl/wiremock/mapping/MappingBuilderExtensionTest.kt
@@ -0,0 +1,113 @@
+package dsl.wiremock.mapping
+
+import com.github.tomakehurst.wiremock.client.MappingBuilder
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.matching.EqualToJsonPattern
+import com.github.tomakehurst.wiremock.matching.RegexPattern
+import com.github.tomakehurst.wiremock.matching.UrlPattern
+import dsl.wiremock.request.Cookie
+import dsl.wiremock.request.Header
+import dsl.wiremock.request.Parameter
+import dsl.wiremock.request.RequestBodyPattern
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.function.Consumer
+
+internal class MappingBuilderExtensionTest {
+
+    lateinit var builder: MappingBuilder
+
+    @BeforeEach
+    fun init() {
+        builder = WireMock.any(UrlPattern.ANY)
+        val responseBuilder = ResponseDefinitionBuilder().withStatus(200)
+        builder.willReturn(responseBuilder)
+    }
+
+    @Test
+    fun `withHeaders should add headers patterns to the mapping builder`() {
+
+        val headerOne = Header("Accept", WireMock.equalTo("plain/text"))
+        val headerTwo = Header("Authorization", WireMock.equalTo("Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="))
+
+        builder.withHeaders(listOf(headerOne, headerTwo))
+
+        val stub = builder.build()
+
+        assertThat(stub.request.headers).containsKey("Accept")
+        assertThat(stub.request.headers["Accept"]).satisfies(Consumer {
+            assertThat(it?.valuePattern?.value).isEqualTo("plain/text")
+        })
+
+        assertThat(stub.request.headers).containsKey("Authorization")
+        assertThat(stub.request.headers["Authorization"]).satisfies(Consumer {
+            assertThat(it?.valuePattern?.value).isEqualTo("Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==")
+        })
+    }
+
+    @Test
+    fun `withCookies should add cookies patterns to the mapping builder`() {
+
+        val cookieOne = Cookie("Session", WireMock.equalTo("SkjdhasdHH"))
+        val cookieTwo = Cookie("Version", WireMock.equalTo("1.0"))
+
+        builder.withCookies(listOf(cookieOne, cookieTwo))
+
+        val stub = builder.build()
+
+        assertThat(stub.request.cookies).containsKey("Session")
+        assertThat(stub.request.cookies["Session"]).satisfies(Consumer {
+            assertThat(it?.value).isEqualTo("SkjdhasdHH")
+        })
+
+        assertThat(stub.request.cookies).containsKey("Version")
+        assertThat(stub.request.cookies["Version"]).satisfies(Consumer {
+            assertThat(it?.value).isEqualTo("1.0")
+        })
+    }
+
+    @Test
+    fun `withQueryParams should add query parameters patterns to the mapping builder`() {
+
+        val paramOne = Parameter("stage", WireMock.equalTo("dev"))
+        val paramTwo = Parameter("startDate", WireMock.equalToDateTime("2022-01-16 22:12:00"))
+
+        builder.withQueryParams(listOf(paramOne, paramTwo))
+
+        val stub = builder.build()
+
+        assertThat(stub.request.queryParameters).containsKey("stage")
+        assertThat(stub.request.queryParameters["stage"]).satisfies(Consumer {
+            assertThat(it?.valuePattern?.value).isEqualTo("dev")
+        })
+
+        assertThat(stub.request.queryParameters).containsKey("startDate")
+        assertThat(stub.request.queryParameters["startDate"]).satisfies(Consumer {
+            assertThat(it?.valuePattern?.value).isEqualTo("2022-01-16 22:12:00")
+        })
+    }
+
+    @Test
+    fun `withRequestBodyPatterns should add body patterns to the mapping builder`() {
+
+        val patternOne = RequestBodyPattern().json("""{"key": "value"}""")
+        val patternTwo = RequestBodyPattern().matches(".*value.*")
+
+        builder.withRequestBodyPatterns(listOf(patternOne, patternTwo))
+
+        val stub = builder.build()
+
+        assertThat(stub.request.bodyPatterns).hasSize(2)
+        assertThat(stub.request.bodyPatterns[0]).satisfies(Consumer {
+            assertThat(it).isInstanceOf(EqualToJsonPattern::class.java)
+            assertThat(it.value).isEqualTo("""{"key": "value"}""")
+        })
+
+        assertThat(stub.request.bodyPatterns[1]).satisfies(Consumer {
+            assertThat(it).isInstanceOf(RegexPattern::class.java)
+            assertThat(it.value).isEqualTo(".*value.*")
+        })
+    }
+}
