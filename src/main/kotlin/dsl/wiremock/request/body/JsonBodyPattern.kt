55b4e1c4e0cb0e53b209cb7c98e75e498bc9eb65
Valentin Goncharov <v.goncharov@quin.md>
2022-02-11 20:34:39 +0100

    #1 - fix build

--- src/main/kotlin/dsl/wiremock/request/body/JsonBodyPattern.kt
+++ src/main/kotlin/dsl/wiremock/request/body/JsonBodyPattern.kt
@@ -1,7 +1,6 @@
 package dsl.wiremock.request.body
 
 import com.github.tomakehurst.wiremock.client.WireMock
-import com.github.tomakehurst.wiremock.matching.StringValuePattern
 import dsl.wiremock.WireMockDSL
 
 @WireMockDSL
@@ -10,11 +9,6 @@ class JsonBodyPattern: StringValueRequestBodyPattern, JsonRequestBodyPattern {
     constructor(scope: RequestBodyScope): super(scope)
 
     constructor(pattern: RequestBodyPattern): super(pattern)
-
-    constructor(pattern: RequestBodyPattern, value: String, valuePattern: StringValuePattern ): super(pattern) {
-        this.currentValue = value
-        this.valuePattern = valuePattern
-    }
 
     @WireMockDSL
     override infix fun ignore(fn: JsonIgnoreScope.()->Unit): StringValuePatternWrapper {
