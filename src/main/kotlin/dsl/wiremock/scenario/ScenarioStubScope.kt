b1eb69096a70a654d32548fd20983f228b372891
Valentin Goncharov <goncharov.valentin@gmail.com>
2022-02-16 23:24:51 +0100

    #  - add licence text

--- src/main/kotlin/dsl/wiremock/scenario/ScenarioStubScope.kt
+++ src/main/kotlin/dsl/wiremock/scenario/ScenarioStubScope.kt
@@ -1,3 +1,22 @@
+/*-
+ * ========================LICENSE_START=================================
+ * Wiremock Kotlin DSL
+ * %%
+ * Copyright (C) 2022 Valentin Goncharov
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
 package dsl.wiremock.scenario
 
 import com.github.tomakehurst.wiremock.client.MappingBuilder
@@ -15,8 +34,6 @@ import dsl.wiremock.response.ResponseScope
 import dsl.wiremock.response.withProxy
 import dsl.wiremock.response.withTransformers
 import dsl.wiremock.stubs.StubScope
-import dsl.wiremock.stubs.scenario.ScenarioRequestScope
-import dsl.wiremock.stubs.scenario.ScenarioScope
 import java.util.*
 
 class ScenarioStubScope(
--- src/main/kotlin/dsl/wiremock/stubs/Stubs.kt
+++ src/main/kotlin/dsl/wiremock/stubs/Stubs.kt
@@ -1,3 +1,22 @@
+/*-
+ * ========================LICENSE_START=================================
+ * Wiremock Kotlin DSL
+ * %%
+ * Copyright (C) 2022 Valentin Goncharov
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
 package dsl.wiremock.stubs
 
 import com.github.tomakehurst.wiremock.client.MappingBuilder
@@ -6,7 +25,7 @@ import com.github.tomakehurst.wiremock.junit.Stubbing
 import com.github.tomakehurst.wiremock.matching.UrlPattern
 import dsl.wiremock.WireMockDSL
 import dsl.wiremock.request.RequestScope
-import dsl.wiremock.stubs.scenario.ScenarioScope
+import dsl.wiremock.scenario.ScenarioScope
 
 @WireMockDSL
 fun get(init: RequestScope.() -> Unit) = createRequestScope(WireMock::get, init)
